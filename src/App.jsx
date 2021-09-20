6c607a92248796dd0fb0c56244ccc948b0a3415b
acezoldyck <lamquangkhanh2710@gmail.com>
2021-09-21 01:38:14 +0700

    ok2

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import React, { useState } from "react";
 
 function App() {
   const [age, setAge] = useState();
-  const handClick = () => {
+  const [hinh, setHinh] = useState("");
+  const [mess, setMess] = useState("");
+  const handClick = (e) => {
+    setHinh("https://scontent.fsgn8-1.fna.fbcdn.net/v/t1.18169-9/21150278_1761946690771532_840871712300140804_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=ad2b24&_nc_ohc=SZqMZr8tum4AX-bWSR-&_nc_ht=scontent.fsgn8-1.fna&oh=84a0ed2a0c266f0d773635b340fe71bc&oe=616EE95E")
+    setMess("BỊ LỪA RỒI GIẬN GHÊ")
     alert("Chiều cao của bạn là: " + age + " cm")
   }
   const handleChange = (e) => {
@@ -15,6 +19,10 @@ function App() {
       Nhập chiều cao của bạn:
       <input type="text" id="lname" name="lname" value={age} onChange={handleChange}></input>
       <button type="button" onClick={handClick}>OK</button>
+      <br/><br/><br/>
+      <img src={hinh} alt="" />
+      <br/>
+      <p style={{color:"red", fontSize:"30px"}}>{mess}</p>
     </div>
   );
 }
